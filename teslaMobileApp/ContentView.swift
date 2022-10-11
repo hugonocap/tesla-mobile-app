ae41cdac87976848e81e230a1d20e3428d5bef83
Hugo Montana <benzlbenny2k@gmail.com>
2022-10-11 22:10:39 +0300

    voice command button

--- teslaMobileApp/ContentView.swift
+++ teslaMobileApp/ContentView.swift
@@ -9,12 +9,38 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        ZStack {
+            VoiceCommandButton()
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+
+ 
+
+struct VoiceCommandButton: View {
+    var body: some View {
+        VStack {
+            Spacer()
+            HStack {
+                Spacer()
+                Image(systemName: "mic.fill")
+                    .font(.system(size: 24,
+                                  weight: .semibold,
+                                  design: .default))
+                    .frame(width: 64, height: 64)
+                    .background(.green)
+                    .clipShape(Circle())
+                    .padding()
+                    .shadow(radius: 10)
+            }
+        }
+        .edgesIgnoringSafeArea(.all)
     }
 }
