a82949787e3c444cdedc913839e0a12f74787485
Hugo Montana <benzlbenny2k@gmail.com>
2022-10-15 22:13:10 +0300

    car pin

--- teslaMobileApp/LocationView.swift
+++ teslaMobileApp/LocationView.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 import MapKit
-
+// location struct
 struct CarLocation: Identifiable {
     let id = UUID()
     let latitude: Double
@@ -17,15 +17,23 @@ struct CarLocation: Identifiable {
     }
 }
 
+let carLocations = [CarLocation(latitude: 34.070871, longitude: -118.403738)]
+
 struct LocationView: View {
     
     @Environment(\.presentationMode) var presentationMode
+    
     @State private var location = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 34.070871, longitude: -118.403738), span: MKCoordinateSpan(latitudeDelta: 0.001, longitudeDelta: 0.001))
     
     var body: some View {
         ZStack {
             // map 
-            Map(coordinateRegion: $location)
+            Map(coordinateRegion: $location,
+                annotationItems:  carLocations,
+                annotationContent: { location in
+                MapAnnotation(coordinate: location.coordinate, content: {CarPin()})
+            })
+                
             VStack(alignment: .leading, spacing: 10) {
                 // header
                 HStack {
@@ -48,6 +56,25 @@ struct LocationView: View {
         .foregroundColor(.white)
         .navigationTitle("Mach Five")
         .navigationBarHidden(true)
+    }
+}
+                
+struct CarPin: View {
+   var body: some View {
+       VStack(alignment: .center, spacing: 4 ) {
+           Image(systemName: "car.fill")
+               .frame(width: 36, height: 36, alignment: .center)
+               .background(Color("Red"))
+               .foregroundColor(.white)
+               .clipShape(Circle())
+           Text("Mach Five")
+               .font(.footnote)
+               .fontWeight(.medium)
+               .padding(.horizontal, 8)
+               .padding(.vertical, 4)
+               .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 8, style: .continuous))
+               .overlay(RoundedRectangle(cornerRadius: 8, style: .continuous).stroke(.black.opacity(0.1), lineWidth: 1))
+       }
     }
 }
 
